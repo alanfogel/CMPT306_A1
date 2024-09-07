f7019ff546999bbae35e81a3e27371d8a2c9155e
Alan Fogel <alanjfogel@gmail.com>
2024-09-07 16:32:14 -0600

    Step 7, added Obstacle with collision, made it start large and shrink

---
+++ obstacle.gd
@@ -0,0 +1,19 @@
+extends AnimatableBody2D
+class_name Obstacle
+
+@export var shrink_speed := 3.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$CollisionPolygon2D.scale = Vector2.ONE*10.0
+
+func _physics_process(delta: float) -> void:
+	$CollisionPolygon2D.scale -= Vector2.ONE * shrink_speed*delta
+	
+	#once tiny, delete
+	if $CollisionPolygon2D.scale.x < .05:
+		self.queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
