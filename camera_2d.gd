6d65d270d8d29d78f97a47a8929cd7af840b8cdc
Alan Fogel <alanjfogel@gmail.com>
2024-09-07 18:04:57 -0600

    Step 10, setup camera rotation, and looped animated background to cycle through more colors

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-var spin_speed := 10
+@export var rotation_speed := 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	rotation += rotation_speed*delta
